68cef3bc19faee0134291911b669ba782553efc8
brookspeppin <bpeppin@vmware.com>
2019-07-25 08:49:18 -0600

    Update Tracker.ps1
    added IE icon copy

--- Tracker/Tracker.ps1
+++ Tracker/Tracker.ps1
@@ -1,1 +1,4 @@
+while(-not(test-path '%appdata%\Microsoft\Windows\Start Menu\Programs\Accessories\Internet Explorer.lnk')){start-sleep 3}
+copy-item '%appdata%\Microsoft\Windows\Start Menu\Programs\Accessories\Internet Explorer.lnk' 'c:\programdata\Microsoft\Windows\Start Menu\Programs\' -ErrorAction SilentlyContinue
+
 while(-not $completed){if((get-itemproperty -path 'HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Main' -ErrorAction SilentlyContinue).'Start Page' -eq 'https://mgs.marriott.com' -and (get-itemproperty -path 'HKLM:\SOFTWARE\CrowdStrike\Falcon' -ErrorAction SilentlyContinue).Status -eq '0' -and (Test-Path "C:\Program Files (x86)\VMware\VMware Horizon View Client\vmware-view.exe") -and (Test-Path "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\Internet Explorer.lnk")){$completed = $true; shutdown /r /t 3}else{echo 'Waiting';start-sleep 3}}
