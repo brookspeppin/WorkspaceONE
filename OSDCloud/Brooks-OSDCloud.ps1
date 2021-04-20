088e56dc777ee5aa10d7b1b937b00b7c04b86b97
brookspeppin <33036379+brookspeppin@users.noreply.github.com>
2021-04-19 20:31:28 -0600

    Update Brooks-OSDCloud.ps1

--- OSDCloud/Brooks-OSDCloud.ps1
+++ OSDCloud/Brooks-OSDCloud.ps1
@@ -1,4 +1,4 @@
-Write-Host  -ForegroundColor Cyan "Starting Brooks OSD's Custom OSDCloud ..."
+Write-Host  -ForegroundColor Cyan "Starting Brooks' Custom OSDCloud ..."
 Start-Sleep -Seconds 5
 
 #Change Display Resolution for Virtual Machine
@@ -8,10 +8,11 @@ if ((Get-MyComputerModel) -match 'Virtual') {
 }
 
 #Make sure I have the latest OSD Content
-Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
-Install-Module OSD -Force
+#Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
+
 
 Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
+Install-Module OSD -Force
 Import-Module OSD -Force
 
 #TODO: Spend the time to write a function to do this and put it here
@@ -21,14 +22,15 @@ Write-Warning "That didn't work because I haven't coded it yet!"
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
-#Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
-Start-OSDCloud -OSLanguage en-us
+Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
+#Start-OSDCloud -OSLanguage en-us
 
 #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
-Write-Warning "I'm not sure of what to put here yet"
+x:\OSDCloud\Autopilot\DownloadFilesFromRepo.ps1
+
 
 #Restart from WinPE
-Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
-Start-Sleep -Seconds 20
+Write-Host  -ForegroundColor Cyan "Restarting in 5 seconds!"
+Start-Sleep -Seconds 5
 wpeutil reboot
