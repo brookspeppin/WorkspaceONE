f33266971a0220b9a4ce2548aef8366f9a993881
brookspeppin <33036379+brookspeppin@users.noreply.github.com>
2021-05-10 21:07:51 -0600

    Menu

--- OSDCloud/Brooks-OSDCloud.ps1
+++ OSDCloud/Brooks-OSDCloud.ps1
@@ -6,24 +6,35 @@ if ((Get-MyComputerModel) -match 'Virtual') {
     Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
     Set-DisRes 1600
 }
-
-#Make sure I have the latest OSD Content
-#Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
-
-
-Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
+Write-Host  -ForegroundColor Cyan "Importing the OSDCloud PowerShell Module"
 Install-Module OSD -Force
 Import-Module OSD -Force
 
-#TODO: Spend the time to write a function to do this and put it here
-#Write-Host  -ForegroundColor Cyan "Ejecting ISO"
-#Write-Warning "That didn't work because I haven't coded it yet!"
-#Start-Sleep -Seconds 5
+if ($os.name -eq "|x:\windows|")
+	{
+		
+	write-host BIOS Version: UEFI -ForegroundColor yellow
+		
+	}
+	
+	Write-Host "================== Main Menu =========================="
+	Write-Host "===============www.brookspeppin.com ================="
+	Write-Host "===================@brookspeppin ===================="
+	Write-Host "========By Brooks Peppin (bpeppin@vmware.com)=========="
+	Write-Host "======================================================="`n
+	Write-Host "1: Zero-Touch Win10 Latest | English | Enterprise"
+	Write-Host "2: Zero-Touch Win10 n-1 | English | Enterprise"
+	Write-Host "3: I'll select it myself"
+	Write-Host "4: Exit`n"
+	$input = Read-Host "Please make a selection"
+	switch ($input)
+	{
+		'1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI } #Call Image_from_network function
+		'2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI } #Call Image_from_Network function
+		'3' { Start-OSDCloud	} #Call Image_from_Network function
+		'4' { Exit		}
+	}
 
-#Start OSDCloud ZTI the RIGHT way
-Write-Host  -ForegroundColor Cyan "Start OSDCloud with Parameters: 20H2, ENT, Zero-Touch"
-Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
-#Start-OSDCloud -OSLanguage en-us
 
 #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
