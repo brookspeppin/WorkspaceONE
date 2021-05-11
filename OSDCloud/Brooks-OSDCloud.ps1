610c5077192133ac5059a4a82fb5154bda0b1d39
brookspeppin <33036379+brookspeppin@users.noreply.github.com>
2021-05-10 21:22:19 -0600

    cleaned up

--- OSDCloud/Brooks-OSDCloud.ps1
+++ OSDCloud/Brooks-OSDCloud.ps1
@@ -1,28 +1,19 @@
 Write-Host  -ForegroundColor Cyan "Starting Brooks' Custom OSDCloud ..."
-Start-Sleep -Seconds 5
 
+Write-Host "================== Main Menu =========================="
+Write-Host "===============www.brookspeppin.com ================="
+Write-Host "===================@brookspeppin ===================="
+Write-Host "========By Brooks Peppin (bpeppin@vmware.com)=========="
+Write-Host "======================================================="`n
+Write-Host "1: Zero-Touch Win10 21H1 | English | Enterprise"
+Write-Host "2: Zero-Touch Win10 20H2 | English | Enterprise"
+Write-Host "3: I'll select it myself"
+Write-Host "4: Exit`n"
+$input = Read-Host "Please make a selection"
+Write-Host  -ForegroundColor Cyan "Starting OSDCloud PowerShell Module"
 
-Write-Host  -ForegroundColor Cyan "Importing the OSDCloud PowerShell Module"
-Install-Module OSD -Force
 Import-Module OSD -Force
-
-if ($os.name -eq "|x:\windows|")
-	{
-		
-	write-host BIOS Version: UEFI -ForegroundColor yellow
-		
-	}
-	
-	Write-Host "================== Main Menu =========================="
-	Write-Host "===============www.brookspeppin.com ================="
-	Write-Host "===================@brookspeppin ===================="
-	Write-Host "========By Brooks Peppin (bpeppin@vmware.com)=========="
-	Write-Host "======================================================="`n
-	Write-Host "1: Zero-Touch Win10 Latest | English | Enterprise"
-	Write-Host "2: Zero-Touch Win10 n-1 | English | Enterprise"
-	Write-Host "3: I'll select it myself"
-	Write-Host "4: Exit`n"
-	$input = Read-Host "Please make a selection"
+Install-Module OSD -Force
 	switch ($input)
 	{
 		'1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI } 
@@ -32,25 +23,7 @@ if ($os.name -eq "|x:\windows|")
 	}
 
 
-#Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
-Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
-#x:\OSDCloud\Autopilot\Profiles\DownloadFilesFromRepo.ps1
-#$USB_Boot = ((Get-Volume).where({ $_.FileSystemLabel -eq "OSDCloud" })).DriveLetter + ":"
-$usb = ((Get-Volume).where({ ($_.FileSystemLabel -eq "OSDCloud") -and ($_.DriveType -eq "Fixed") })).DriveLetter + ":"
-#$usb_source = ((Get-Volume).where({ $_.FileSystemLabel -eq "USB-Source" })).DriveLetter + ":"
-$path = "C:\programdata\vmware\"
-if(!(Test-Path $path)){
-    mkdir $path
-}
-$filename = "AwProvAgent-x64.msi"
-Copy-Item -Path $usb\ws1\$filename -Destination $path 
-
-Copy-Item -Path $usb\ws1\VMwareWS1ProvisioningTool.exe -Destination $path
-Copy-Item -Path $usb\ws1\VMwareWS1ProvisioningTool.exe.config -Destination $path
-Copy-Item -Path $usb\ws1\unattend_OSDCloud.xml -Destination C:\Windows\Panther\Invoke-OSDSpecialize.xml -Force
-
 
 #Restart from WinPE
-#Write-Host  -ForegroundColor Cyan "Restarting in 5 seconds!"
-#pause
+
 wpeutil reboot
