991ed27cdc6fae8d152f2ef3fb3eb7aeb43fd113
brookspeppin <33036379+brookspeppin@users.noreply.github.com>
2021-05-11 21:23:33 -0600

    fix

--- OSDCloud/Brooks-OSDCloud.ps1
+++ OSDCloud/Brooks-OSDCloud.ps1
@@ -1,28 +1,26 @@
 Write-Host  -ForegroundColor Yellow "Starting Brooks' Custom OSDCloud ..."
-
-Write-Host "================== Main Menu ==========================" -ForegroundColor Yellow
+cls
+Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
 Write-Host "================ www.brookspeppin.com =================" -ForegroundColor Yellow
-Write-Host "===================@brookspeppin ======================" -ForegroundColor Yellow
+Write-Host "====================@brookspeppin =====================" -ForegroundColor Yellow
 Write-Host "=======================================================" -ForegroundColor Yellow
 Write-Host "1: Zero-Touch Win10 21H1 | English | Enterprise"-ForegroundColor Yellow
 Write-Host "2: Zero-Touch Win10 20H2 | English | Enterprise" -ForegroundColor Yellow
 Write-Host "3: I'll select it myself"-ForegroundColor Yellow
 Write-Host "4: Exit`n"-ForegroundColor Yellow
-$input = Read-Host "Please make a selection"-ForegroundColor Yellow
-Write-Host  -ForegroundColor Yellow "Starting OSDCloud PowerShell Module"
+$input = Read-Host "Please make a selection"
+
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
 
 Import-Module OSD -Force
 Install-Module OSD -Force
-	switch ($input)
-	{
-		'1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI } 
-		'2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
-		'3' { Start-OSDCloud	} 
-		'4' { Exit		}
-	}
 
-
-
-#Restart from WinPE
+switch ($input)
+{
+    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
+    '3' { Start-OSDCloud	} 
+    '4' { Exit		}
+}
 
 wpeutil reboot
